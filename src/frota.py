abce30d8b34d5bce8f17a14eeee3083cfcdbd88b
ceitelabinfo <teste@teste.com>
2024-08-23 20:31:17 -0300

    Adição do tanque e do consumo médio.

--- src/frota.py
+++ src/frota.py
@@ -3,25 +3,38 @@ class Carro:
     marca : str
     cor : str
     odometro : 0.0
-    motor_on : False
+    motor_on = False
+    tanque = float
+    consumo_medio = float
 
     def __init__(self, modelo : str, marca : str, cor : str,
-                       odometro : float, motor : bool):
+                       odometro : float, motor : bool,
+                       tanque : float, cm : float):
         self.modelo = modelo
         self.marca = marca
         self.cor = cor
         self.odometro = odometro
         self.motor_on = motor
+        self.tanque = tanque
+        self.consumo_medio = cm
 
     def ligar(self):
-        if not self.motor_on:
+        if not self.motor_on and self.tanque > 0:
             self.motor_on = True
         else:
             raise Exception("Erro: Motor já ligado!")
 
     def acelerar(self, velocidade : float, tempo : float):
         if self.motor_on:
-            self.odometro += velocidade * tempo
+            km = velocidade * tempo
+            litros = km / self.consumo_medio
+            if self.tanque >= litros:
+                self.tanque -= litros
+            else:
+                km = self.tanque * self.consumo_medio
+                self.tanque = 0
+                self.desligar()
+            self.odometro += km
         else:
             raise Exception("Erro: Não é possível acelerar! Motor desligado!")
 
@@ -34,7 +47,8 @@ class Carro:
     def __str__(self):
         info = (f'Carro {self.modelo}, marca {self.marca}, '
                 f'cor {self.cor}\n{self.odometro} Km, '
-                f'motor {self.motor_on}')
+                f'motor {self.motor_on} consumo {self.consumo_medio} Km/l '
+                f'nível do tanque em {self.tanque} L')
         return info
 
 
